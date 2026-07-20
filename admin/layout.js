4df06be38e73087c65b05086373451f5d8c75003
vozalasnaciones <adodiaz51@gmail.com>
2026-07-20 13:50:38 -0600

    Update layout.js

--- admin/layout.js
+++ admin/layout.js
@@ -1,1 +1,27 @@
+function renderLayout(){
 
+    document.getElementById("app").innerHTML=`
+
+<div class="layout">
+
+    <aside id="sidebar"></aside>
+
+    <main>
+
+        <header id="header"></header>
+
+        <section id="content"></section>
+
+    </main>
+
+</div>
+
+`;
+
+    renderSidebar();
+
+    renderHeader();
+
+    renderLibrary();
+
+}
