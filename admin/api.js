c76cf72fa2ccb057c0859320f0e3cdf24ae52132
vozalasnaciones <adodiaz51@gmail.com>
2026-07-20 13:27:58 -0600

    Update api.js

--- admin/api.js
+++ admin/api.js
@@ -1,1 +1,9 @@
+const API = "https://marketingapi.adodiaz51.workers.dev";
 
+async function getFiles() {
+
+    const response = await fetch(API + "/files");
+
+    return await response.json();
+
+}
